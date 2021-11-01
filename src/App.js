9ff099a031c2432661e92848ad1bae65400430e8
Saarthak <zsaarthakz@gmail.com>
2021-11-01 12:09:02 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import * as Three from "three";
 export default function App() {
 
   const scene = new Three.Scene();
+  scene.background = new Three.Color("rgb(255, 255, 255)");
   const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new Three.WebGLRenderer();
   const canvasDivRef = useRef(null);
+
 
   renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -15,19 +17,84 @@ export default function App() {
   useEffect(() => {
     canvasDivRef.current.appendChild(element);
 
-    const geometry = new Three.BoxGeometry();
-    const material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new Three.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
+    const planeGeometry = new Three.PlaneGeometry(10, 10);
+    const planeMaterial = new Three.MeshBasicMaterial({
+      side: Three.DoubleSide,
+      color: new Three.Color("rgb(0, 0, 0)")
+    });
+    const planeMesh = new Three.Mesh(planeGeometry, planeMaterial);
+
+    const boxGeometry = new Three.BoxGeometry(2, 2, 2);
+    const boxMaterial = new Three.MeshBasicMaterial({
+      color: new Three.Color("rgb(200, 0, 0)")
+    });
+    const boxMesh = new Three.Mesh(boxGeometry, boxMaterial);
+
+    scene.add(planeMesh);
+    scene.add(boxMesh);
+
+    planeMesh.rotateX(Math.PI / 2);
+    boxMesh.translateY(1);
+
+    camera.position.z = 10;
+
+    const keys = {
+      Up: false,
+      Down: false,
+      Space: false
+    };
+
+    document.addEventListener("keydown", (event) => {
+      if (event.key == "ArrowUp") {
+        keys.Up = true;
+      };
+
+      if (event.key == "ArrowDown") {
+        keys.Down = true;
+      };
+
+      if (event.key == "ArrowLeft") {
+        camera.translateX(0.1);
+      };
+
+      if (event.key == "ArrowRight") {
+        camera.translateX(-0.1);
+      };
+
+      if (event.key == " ") {
+        keys.Space = true;
+      };
+
+      if (keys.Space && keys.Up) {
+        camera.translateZ(0.1);
+      } else if (keys.Up) {
+        camera.translateY(0.1);
+      }
+
+      if (keys.Space && keys.Down) {
+        camera.translateZ(-0.1);
+      } else if (keys.Down) {
+        camera.translateY(-0.1);
+      }
+    });
+
+    document.addEventListener("keyup", (event) => {
+      if (event.key == "ArrowUp") {
+        keys.Up = false;
+      };
+
+      if (event.key == "ArrowDown") {
+        keys.Down = false;
+      };
+
+      if (event.key == " ") {
+        keys.Space = false;
+      };
+    });
 
     function animate() {
-      requestAnimationFrame(animate);
       renderer.render(scene, camera);
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
+      requestAnimationFrame(animate);
     }
 
     animate();
